b2c826b2a0ed05051500784865419399c98e6026
Rafael Carneiro <rafaeldelima97@hotmail.com>
2023-01-27 21:14:21 -0300

    aula 2 active mq

--- src/main/java/dev/fujioka/java/avancado/web/WebApplication.java
+++ src/main/java/dev/fujioka/java/avancado/web/WebApplication.java
@@ -2,8 +2,10 @@ package dev.fujioka.java.avancado.web;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jms.annotation.EnableJms;
 
 @SpringBootApplication
+@EnableJms
 public class WebApplication{
 
     public static void main(String[] args) {
